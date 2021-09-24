0f66fc635978f0524887b307e04329d01dfa4e61
Sergey Gnid <serhii.hnid@gmail.com>
2021-09-24 16:26:11 +0300

    1. Example Task

--- BaseUI/BaseUI/ViewController.swift
+++ BaseUI/BaseUI/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onClick(_ sender: Any) {
+        label.isHidden.toggle()
+    }
 }
 
