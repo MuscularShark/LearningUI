b6b115ff868049fec4631e05c0b21f5dd2b81cce
Sergey Gnid <serhii.hnid@gmail.com>
2021-09-27 02:15:25 +0300

    3. Change color with textfields

--- BaseUI/BaseUI/ViewController.swift
+++ BaseUI/BaseUI/ViewController.swift
@@ -10,26 +10,31 @@ import UIKit
 class ViewController: UIViewController {
 
 
-    @IBOutlet weak var valueOfRedSlider: UILabel!
+    @IBOutlet weak var redTextField: UITextField!
     
     @IBOutlet weak var redSlider: UISlider!
     
-    @IBOutlet weak var valueOfGreenSlider: UILabel!
+    @IBOutlet weak var greenTextField: UITextField!
     
     @IBOutlet weak var greenSlider: UISlider!
     
-    @IBOutlet weak var valueOfBlueSlider: UILabel!
+    @IBOutlet weak var blueTextfield: UITextField!
     
     @IBOutlet weak var blueSlider: UISlider!
     
     @IBOutlet weak var viewOfColors: UIView!
     
 /// RGB Colors parametrs
+    
     var redColor: Float = 0
     
     var greenColor: Float = 0
     
     var blueColor: Float = 0
+    
+/// Constant for max range RGB colors
+    
+    let valueCG: CGFloat = 255
     
     
     
@@ -42,53 +47,55 @@ class ViewController: UIViewController {
         changeColorView(red: redColor, green: greenColor, blue: blueColor)
         changeText(nil)
         changeColorOfSlider(nil)
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextfield.delegate = self
     }
     
 // MARK: - Changes to the view regarding colors.
     /// This method change background color for attached view for specific slider.
     ///
-    /// - Parameter red: Value of red color in the range 0 - 1 from UIColor.
-    /// - Parameter green: Value of green color in the range 0 - 1 from UIColor.
-    /// - Parameter blue: Value of blue color in the range 0 - 1 from UIColor.
+    /// - Parameter red: Value of red color in the range 0 - 255 from UIColor.
+    /// - Parameter green: Value of green color in the range 0 - 255 from UIColor.
+    /// - Parameter blue: Value of blue color in the range 0 - 255 from UIColor.
     
-    
-    func changeColorView(red: Float, green: Float, blue: Float) {
+    private func changeColorView(red: Float, green: Float, blue: Float) {
         if red != 0 || green != 0 || blue != 0 {
             redColor = red
             greenColor = green
             blueColor = blue
         }
         let color = UIColor(
-            red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1.0
+            red: CGFloat(redColor) / valueCG, green: CGFloat(greenColor) / valueCG, blue: CGFloat(blueColor) / valueCG, alpha: 1.0
         )
         viewOfColors.backgroundColor = color
     }
 
 //MARK: - Changes to text relative to slider values.
-    /// This method change text for attached label for specific slider.
+    /// This method change text for attached textfield for specific slider.
     ///
     /// - Parameter sender: The specific slider.
     
-    func changeText(_ sender: UISlider?){
+    private func changeText(_ sender: UISlider?){
         if let sender = sender{
             switch sender.tag {
             case 1:
-                valueOfRedSlider.text = String(sender.value)
+                redTextField.text = String(Int(sender.value))
                 
             case 2:
-                valueOfGreenSlider.text = String(sender.value)
+                greenTextField.text = String(Int(sender.value))
                 
             case 3:
-                valueOfBlueSlider.text = String(sender.value)
+                blueTextfield.text = String(Int(sender.value))
                 
             default:
                 print("The specified tag could not be found")
             }
         }
         else {
-            valueOfRedSlider.text = String(redSlider.value)
-            valueOfGreenSlider.text = String(greenSlider.value)
-            valueOfBlueSlider.text = String(blueSlider.value)
+            redTextField.text = String(Int(redSlider.value))
+            greenTextField.text = String(Int(greenSlider.value))
+            blueTextfield.text = String(Int(blueSlider.value))
         }
     }
     
@@ -97,26 +104,26 @@ class ViewController: UIViewController {
     ///
     /// - Parameter sender: The specific slider.
     
-    func changeColorOfSlider(_ sender: UISlider?) {
+    private func changeColorOfSlider(_ sender: UISlider?) {
         if let sender = sender{
             switch sender.tag {
             case 1:
-                redSlider.tintColor = UIColor(red: CGFloat(sender.value), green: 0, blue: 0, alpha: 1)
+                redSlider.tintColor = UIColor(red: CGFloat(sender.value) / valueCG, green: 0, blue: 0, alpha: 1)
                 
             case 2:
-                greenSlider.tintColor = UIColor(red: 0, green: CGFloat(sender.value), blue: 0, alpha: 1)
+                greenSlider.tintColor = UIColor(red: 0, green: CGFloat(sender.value) / valueCG, blue: 0, alpha: 1)
                 
             case 3:
-                blueSlider.tintColor = UIColor(red: 0, green: 0, blue: CGFloat(sender.value), alpha: 1)
+                blueSlider.tintColor = UIColor(red: 0, green: 0, blue: CGFloat(sender.value) / valueCG, alpha: 1)
                 
             default:
                 print("The specified tag could not be found")
             }
         }
         else {
-            redSlider.tintColor = UIColor(red: CGFloat(redSlider.value), green: 0, blue: 0, alpha: 1)
-            greenSlider.tintColor = UIColor(red: 0, green: CGFloat(greenSlider.value), blue: 0, alpha: 1)
-            blueSlider.tintColor = UIColor(red: 0, green: 0, blue: CGFloat(blueSlider.value), alpha: 1)
+            redSlider.tintColor = UIColor(red: CGFloat(redSlider.value) / valueCG, green: 0, blue: 0, alpha: 1)
+            greenSlider.tintColor = UIColor(red: 0, green: CGFloat(greenSlider.value) / valueCG, blue: 0, alpha: 1)
+            blueSlider.tintColor = UIColor(red: 0, green: 0, blue: CGFloat(blueSlider.value) / valueCG, alpha: 1)
         }
     }
     
@@ -145,6 +152,41 @@ class ViewController: UIViewController {
         default:
             print("The specified tag could not be found")
         }
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+// MARK: - Settings for textfields
+    /// This method set restriction for textfield
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            guard let text = textField.text,
+                  let textValue = Float(text + string)
+            else { return false }
+            if textValue > 255 {
+                return false
+            }
+            return true
+        }
+        
+// MARK: - Change color view with textfields
+    /// This method set sliders values as textfields text and chande color for view
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField.tag {
+        case 1:
+            redSlider.value = Float(redTextField.text ?? " ") ?? 0.0
+        case 2:
+            greenSlider.value = Float(greenTextField.text ?? " ") ?? 0.0
+        case 3:
+            blueSlider.value = Float(blueTextfield.text ?? " ") ?? 0.0
+        default:
+            print("Error")
+        }
+        changeColorView(red: Float(redSlider.value), green: Float(greenSlider.value), blue: Float(blueSlider.value))
+        textField.resignFirstResponder()
+        return true
     }
 }
     
