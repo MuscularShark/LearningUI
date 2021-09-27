a0cc06465e6b8bf8d77373c8fed320df86067f5b
Sergey Gnid <serhii.hnid@gmail.com>
2021-09-27 15:51:03 +0300

    Picker ready

--- BaseUIPicker/BaseUIPicker/ViewController.swift
+++ BaseUIPicker/BaseUIPicker/ViewController.swift
@@ -11,30 +11,67 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var textFieldGender: UITextField!
     
+    @IBOutlet weak var textFieldBirth: UITextField!
+    
     @IBOutlet weak var profileImage: UIImageView!
     
-    var pickerGender = UIPickerView()
+    let pickerGender = UIPickerView()
+    
+    let pickerBirhday = UIDatePicker()
+    
+    ///Values for UIPickerView (pickerGender)
     
     var arrayOfGender = ["Female", "Male", "Unknown"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        profileImage.layer.cornerRadius = 55
-        profileImage.clipsToBounds = true
         pickerGender.dataSource = self
         pickerGender.delegate = self
         textFieldGender.inputView = pickerGender
+        textFieldBirth.inputView = pickerBirhday
+        datePickerView()
+        changeImage()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
         textFieldGender.textColor = .black
+        textFieldBirth.textColor = .black
+    }
+    
+    //MARK: - Method for change Image View
+    
+    func changeImage() {
+        profileImage.layer.cornerRadius = profileImage.frame.width / 2
+        profileImage.contentMode = .scaleAspectFit
+    }
+    
+    //MARK: - Method of DatePicker
+    
+    func datePickerView() {
+        textFieldBirth.inputView = pickerBirhday
+        pickerBirhday.datePickerMode = .date
+        pickerBirhday.preferredDatePickerStyle = .wheels
+        pickerBirhday.maximumDate = Date()
+        pickerBirhday.addTarget(self, action: #selector(dateChanged), for: .valueChanged)
+        textFieldBirth.textColor = .systemBlue
+    }
+        
+    //MARK: - Method of date format
+    
+    @objc private func dateChanged() {
+        let dateFormat = DateFormatter()
+        dateFormat.dateStyle = .medium
+        textFieldBirth.text = dateFormat.string(from: pickerBirhday.date)
     }
     
 }
 
 extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    //MARK: - Methods of UIPickerViewDelegate, UIPickerViewDataSource
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
