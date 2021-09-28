78c70726642217ebc2bb329f8a601bb64a6a3114
Sergey Gnid <serhii.hnid@gmail.com>
2021-09-28 11:38:04 +0300

    Initial project

---
+++ LearningUI/LearningUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearningUI
+//
+//  Created by Сергей Гнидь on 28.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
