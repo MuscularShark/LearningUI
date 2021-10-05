6d0a23700cf849a24f9dadeb7cb8161d5ff6ad1d
Sergey Gnid <serhii.hnid@gmail.com>
2021-10-05 14:27:49 +0300

    Some Update

--- LearningUI/LearningUI/ViewControllers/ViewController3.swift
+++ LearningUI/LearningUI/ViewControllers/ViewController3.swift
@@ -12,7 +12,6 @@ class ThirdUIViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 250, height: 27))
-        
         label.center = CGPoint(x: view.frame.width / 2, y: view.frame.height * 0.1)
         label.textAlignment = .center
         label.text = "Third ViewController"
