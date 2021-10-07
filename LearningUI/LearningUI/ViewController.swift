487e28cd837bbcaca4800a1507f951248c69c755
Sergey Gnid <serhii.hnid@gmail.com>
2021-10-07 18:24:16 +0300

    Shape - FIX

--- LearningUI/LearningUI/ViewController.swift
+++ LearningUI/LearningUI/ViewController.swift
@@ -10,20 +10,20 @@ import UIKit
 class ViewController: UIViewController {
     
     private func drawStar() {
-        let startWith = self.view.frame.width
-        let startHeight = self.view.frame.height
+        let startWidth = view.frame.width / 2
+        let startHeight = view.frame.height / 2
         
         let path = UIBezierPath()
-        path.move(to: CGPoint(x: startWith / 2, y: startHeight / 2 - 10))
-        path.addLine(to: CGPoint(x: startWith / 2 + 50, y: startHeight / 2 + 18))    //1 line
-        path.addLine(to: CGPoint(x: startWith / 2 + 100, y: startHeight / 2 - 10))   //2 line
-        path.addLine(to: CGPoint(x: startWith / 2 + 100, y: startHeight / 2 + 50))   //3 line
-        path.addLine(to: CGPoint(x: startWith / 2 + 135, y: startHeight / 2 + 85))   //4 line
-        path.addLine(to: CGPoint(x: startWith / 2 + 80, y: startHeight / 2 + 100))   //5 line
-        path.addLine(to: CGPoint(x: startWith / 2 + 50, y: startHeight / 2 + 150))   //6 line
-        path.addLine(to: CGPoint(x: startWith / 2 + 20, y: startHeight / 2 + 100))   //7 line
-        path.addLine(to: CGPoint(x: startWith / 2 - 40, y: startHeight / 2 + 85))    //8 line
-        path.addLine(to: CGPoint(x: startWith / 2, y: startHeight / 2 + 50))         //9 line
+        path.move(to: CGPoint(x: startWidth, y: startHeight - 10))
+        path.addLine(to: CGPoint(x: startWidth + 50, y: startHeight + 18))    //1 line
+        path.addLine(to: CGPoint(x: startWidth + 100, y: startHeight - 10))   //2 line
+        path.addLine(to: CGPoint(x: startWidth + 100, y: startHeight + 50))   //3 line
+        path.addLine(to: CGPoint(x: startWidth + 135, y: startHeight + 85))   //4 line
+        path.addLine(to: CGPoint(x: startWidth + 80, y: startHeight + 100))   //5 line
+        path.addLine(to: CGPoint(x: startWidth + 50, y: startHeight + 150))   //6 line
+        path.addLine(to: CGPoint(x: startWidth + 20, y: startHeight + 100))   //7 line
+        path.addLine(to: CGPoint(x: startWidth - 40, y: startHeight + 85))    //8 line
+        path.addLine(to: CGPoint(x: startWidth, y: startHeight + 50))         //9 line
         path.close()
 
         let shapeLayer = CAShapeLayer()
@@ -32,16 +32,12 @@ class ViewController: UIViewController {
         shapeLayer.strokeColor = UIColor.black.cgColor
         shapeLayer.fillColor = UIColor.red.cgColor
         shapeLayer.lineWidth = 5
-
         view.layer.addSublayer(shapeLayer)
     }
     
         override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
             drawStar()
     }
-
-
 }
 
