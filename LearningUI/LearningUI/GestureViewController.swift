9d19bfb8f800c92b36dc142028b4db711fff1ba3
Sergey Gnid <serhii.hnid@gmail.com>
2021-10-07 18:19:06 +0300

    Gesture - FIX

--- LearningUI/LearningUI/GestureViewController.swift
+++ LearningUI/LearningUI/GestureViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GestureViewController: UIViewController {
     
     @IBOutlet private weak var redView: UIView!
+    
     @IBOutlet private weak var centerXConstraint: NSLayoutConstraint!
+    
     @IBOutlet private weak var centerYConstraint: NSLayoutConstraint!
     
     private let panGestureRecognizer = UIPanGestureRecognizer()
@@ -23,16 +25,11 @@ class ViewController: UIViewController {
     
     private var panGestureAnchorPoint: CGPoint?
     
-    ///Parametrs of initial position view
     private var defaultStateX: CGFloat?
     private var defaultStateY: CGFloat?
     
-    
-    ///Parametrs of transforms
     private var scale: CGFloat?
     private var rotation: CGFloat?
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,31 +41,22 @@ class ViewController: UIViewController {
 
     }
     
-    //MARK: - Method of setup nessesary gestures
-    
     private func setupGestures() {
-        
-        //Set gestures delegate
         pinchGestureRecognizer.delegate = self
         rotateGestureRecognizer.delegate = self
         
-        //Add gestures target
         panGestureRecognizer.addTarget(self, action: #selector(handPanGesture(_ :)))
         tapGestureRecognizer.addTarget(self, action: #selector(tapGesture(_ :)))
         pinchGestureRecognizer.addTarget(self, action: #selector(pinchGesture(_ :)))
         rotateGestureRecognizer.addTarget(self, action: #selector(rotateGesture(_ :)))
-        
-        //Set touches of gestures
+
         panGestureRecognizer.maximumNumberOfTouches = 1
-        
-        //Add gestures in nesessary view
+
         redView.addGestureRecognizer(panGestureRecognizer)
         redView.addGestureRecognizer(tapGestureRecognizer)
         view.addGestureRecognizer(pinchGestureRecognizer)
         view.addGestureRecognizer(rotateGestureRecognizer)
     }
-    
-    //MARK: - Method of PenGesture
     
     @objc private func handPanGesture(_ gestureRecognizer: UIPanGestureRecognizer) {
         guard panGestureRecognizer === gestureRecognizer else { return }
@@ -83,18 +71,13 @@ class ViewController: UIViewController {
             
             centerXConstraint.constant += gesturePoint.x - panGestureAnchorPoint.x
             centerYConstraint.constant += gesturePoint.y - panGestureAnchorPoint.y
-            
             self.panGestureAnchorPoint = gesturePoint
-            
         case .cancelled, .ended:
             panGestureAnchorPoint = nil
-            
         default:
             break
         }
     }
-    
-    //MARK: - Method of PinchGesture
     
     @objc private func pinchGesture(_ gestureRecognizer: UIPinchGestureRecognizer) {
         guard pinchGestureRecognizer === gestureRecognizer else { return }
@@ -105,13 +88,10 @@ class ViewController: UIViewController {
             if let scales = scale, let rotations = rotation{
                 redView.transform = CGAffineTransform.identity.scaledBy(x: scales, y: scales).rotated(by: CGFloat(rotations))
             }
-            
         default:
             break
         }
     }
-    
-    //MARK: - Method of RotationGesture
     
     @objc private func rotateGesture(_ gestureRecognizer: UIRotationGestureRecognizer) {
         guard rotateGestureRecognizer === gestureRecognizer else { return }
@@ -119,17 +99,13 @@ class ViewController: UIViewController {
         switch rotateGestureRecognizer.state {
         case .changed:
             rotation = gestureRecognizer.rotation
-            
             if let rotations = rotation, let scales = scale {
                 redView.transform = CGAffineTransform.identity.scaledBy(x: scales, y: scales).rotated(by: CGFloat(rotations))
             }
-            
         default:
             break
         }
     }
-    
-   //MARK: - Method of TapGesture
     
     @objc private func tapGesture(_ gestureRecognizer: UITapGestureRecognizer) {
         guard tapGestureRecognizer === gestureRecognizer else { return }
@@ -142,16 +118,13 @@ class ViewController: UIViewController {
                 redView.transform = .identity
             }
             redView.backgroundColor = .green
-            
         default:
             break
         }
     }
-    
-
 }
 
-extension ViewController: UIGestureRecognizerDelegate {
+extension GestureViewController: UIGestureRecognizerDelegate {
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         let simultaneousRecognizers = [panGestureRecognizer, pinchGestureRecognizer, rotateGestureRecognizer]
         return simultaneousRecognizers.contains(gestureRecognizer) && simultaneousRecognizers.contains(otherGestureRecognizer)
