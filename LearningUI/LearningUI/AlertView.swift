0160ae082583d5bd7f9097360ce89614a37e1a93
Sergey Gnid <serhii.hnid@gmail.com>
2021-10-18 17:32:13 +0300

    1. Create xib

---
+++ LearningUI/LearningUI/AlertView.swift
@@ -0,0 +1,19 @@
+//
+//  AlertView.swift
+//  LearningUI
+//
+//  Created by Сергей Гнидь on 18.10.2021.
+//
+
+import UIKit
+
+class AlertView: UIView {
+
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var message: UILabel!
+    
+    
+    @IBAction func clickOk(_ sender: UIButton) {
+    }
+}
