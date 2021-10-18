ad68dfd1c8e73130722ed78e206cdf2be79bb904
Sergey Gnid <serhii.hnid@gmail.com>
2021-10-18 16:37:35 +0300

    Update 18.10

--- LearningUI/LearningUI/ViewController.swift
+++ LearningUI/LearningUI/ViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var seconCard: UIImageView!
+    @IBOutlet private weak var seconCard: UIImageView!
     
-    @IBOutlet weak var firstCard: UIImageView!
+    @IBOutlet private weak var firstCard: UIImageView!
     
-    @IBOutlet weak var backCard: UIImageView!
+    @IBOutlet private weak var backCard: UIImageView!
     
-    @IBOutlet weak var cardView: UIView!
+    @IBOutlet private weak var cardView: UIView!
     
-    @IBOutlet weak var constraintY: NSLayoutConstraint!
+    @IBOutlet private weak var constraintY: NSLayoutConstraint!
     
-    @IBOutlet weak var constraintX: NSLayoutConstraint!
+    @IBOutlet private weak var constraintX: NSLayoutConstraint!
     
-    @IBOutlet weak var hints: UILabel!
+    @IBOutlet private weak var hints: UILabel!
     
-    @IBOutlet weak var tryBtn: UIButton!
+    @IBOutlet private weak var tryBtn: UIButton!
     
     private var defaultX: CGFloat = 0
     
@@ -37,19 +37,29 @@ class ViewController: UIViewController {
     
     private var panGestureAnchorPoint: CGPoint?
     
+    private let valueOut: CGFloat = 480
+    
+    private enum State {
+        case left
+        case right
+        case center
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setDefaultValue()
+        setupGestures()
+    }
+    
+    private func setDefaultValue() {
         defaultX = constraintX.constant
         defaultY = constraintY.constant
-        setupGestures()
     }
 
     private func setupGestures() {
-        //Add gestures target
         tapGestureRecognizer.addTarget(self, action: #selector(tapGesture(_ :)))
         panGestureRecognizer.addTarget(self, action: #selector(panGesture(_ :)))
 
-        //Add gestures in nesessary view
         cardView.addGestureRecognizer(tapGestureRecognizer)
         cardView.addGestureRecognizer(panGestureRecognizer)
     }
@@ -86,72 +96,87 @@ class ViewController: UIViewController {
         })
     }
     
-    @objc private func tapGesture(_ gestureRecognizer: UITapGestureRecognizer) {
-        guard tapGestureRecognizer === gestureRecognizer else { return }
-        
-        guard let second = seconCard, let first = firstCard else { return }
-        
-        switch tapGestureRecognizer.state {
-            case .ended:
-                if coutOfRotate == 0 {
-                    updateCard(card1: second, card2: first, scale: true)
-                    hints.text = "Tap again to close card"
-                    coutOfRotate = 1
-                }
-                else {
-                    updateCard(card1: first, card2: second, scale: false)
-                    hints.text = "You can swipe card left or right"
-                    coutOfRotate = 0
-                }
-            default:
-                break
-        }
-    }
-    
-    @IBAction func clickTry(_ sender: UIButton) {
+    @IBAction private func clickTry(_ sender: UIButton) {
         tryBtn.isHidden = true
         hints.text = "Tap to card"
         constraintX.constant = defaultX
         addCard(card: cardView)
     }
     
+    private func getOut(state: State) {
+        UIView.animate(withDuration: 0.4, animations: {
+            switch state {
+            case .left:
+                self.hints.text = "You swiped card left"
+                self.constraintX.constant -= self.valueOut
+                self.tryBtn.isHidden = false
+            case .right:
+                self.hints.text = "You swiped card right"
+                self.constraintX.constant += self.valueOut
+                self.tryBtn.isHidden = false
+            case .center:
+                self.hints.text = "Not enought to swipe"
+                self.constraintX.constant = self.defaultX
+                self.tryBtn.isHidden = true
+            }
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    @objc private func tapGesture(_ gestureRecognizer: UITapGestureRecognizer) {
+        guard tapGestureRecognizer === gestureRecognizer else { return }
+        
+        guard let second = seconCard, let first = firstCard else { return }
+        
+        switch tapGestureRecognizer.state {
+        case .ended:
+            if coutOfRotate == 0 {
+                updateCard(card1: second, card2: first, scale: true)
+                hints.text = "Tap again to close card"
+                coutOfRotate = 1
+            }
+            else {
+                updateCard(card1: first, card2: second, scale: false)
+                hints.text = "You can swipe card left or right"
+                coutOfRotate = 0
+            }
+        default:
+            break
+        }
+    }
+    
     @objc private func panGesture(_ gestureRecognizer: UITapGestureRecognizer) {
         guard panGestureRecognizer === gestureRecognizer else { return }
         
         switch panGestureRecognizer.state {
-            case .began:
-                panGestureAnchorPoint = gestureRecognizer.location(in: view)
-            case .changed:
-                guard let panGestureAnchorPoint = panGestureAnchorPoint else { return }
+        case .began:
+            panGestureAnchorPoint = gestureRecognizer.location(in: view)
+        case .changed:
+            guard let panGestureAnchorPoint = panGestureAnchorPoint else { return }
             
-                let gesturePoint = gestureRecognizer.location(in: view)
-                constraintX.constant += gesturePoint.x - panGestureAnchorPoint.x
-                self.panGestureAnchorPoint = gesturePoint
+            let gesturePoint = gestureRecognizer.location(in: view)
             
-                if constraintX.constant > 0 {
-                    rotateCard(card: cardView, angle: 0.3)
-                }
-                if constraintX.constant < 0 {
-                    rotateCard(card: cardView, angle: -0.3)
-                }
-            case .ended:
-                if constraintX.constant < -50 {
-                    hints.text = "You swiped card left"
-                    constraintX.constant += -480
-                    tryBtn.isHidden = false
-                }
-                else if constraintX.constant > 50 {
-                    hints.text = "You swiped card right"
-                    constraintX.constant += 480
-                    tryBtn.isHidden = false
-                }
-                else {
-                    hints.text = "Not enought to swipe"
-                    constraintX.constant = defaultX
-                    rotateCard(card: cardView, angle: 0)
-                }
-            default:
-                break
+            constraintX.constant += gesturePoint.x - panGestureAnchorPoint.x
+            self.panGestureAnchorPoint = gesturePoint
+            if constraintX.constant > 0 {
+                rotateCard(card: cardView, angle: 0.3)
+            }
+            if constraintX.constant < 0 {
+                rotateCard(card: cardView, angle: -0.3)
+            }
+        case .ended:
+            if constraintX.constant < -50 {
+                getOut(state: .left)
+            }
+            else if constraintX.constant > 50 {
+                getOut(state: .right)
+            }
+            else {
+                getOut(state: .center)
+                rotateCard(card: cardView, angle: 0)
+            }
+        default:
+            break
         }
     }
 }
